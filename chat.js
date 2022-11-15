2fd5d66f6f59eebb7c1029bd78c580f84b7762ba
Veaceslav <c.veaceslav95@gmail.com>
2022-11-15 15:41:05 +0200

    chat window

---
+++ chat.js
@@ -0,0 +1,57 @@
+const chatWindow = document.getElementById("chat");
+const chatSendBtn = chatWindow.children[2].firstElementChild.lastElementChild;
+const chatInput = document.querySelector("#chat input");
+const chatBody = document.querySelector("#chat .chat-body");
+
+let messages = [];
+
+function initChat() {
+  let chatHidden = !!localStorage.getItem("chatHidden");
+  if (!chatHidden) {
+    openChat();
+  }
+  chatSendBtn.addEventListener("click", sendMessage);
+
+  let message_json = localStorage.getItem("messages");
+  if (message_json != null) {
+    message = JSON.parse(message_json);
+  }
+  renderMessages();
+}
+
+function sendMessage(e) {
+  e.preventDefault();
+
+  // HW:6 uite pe video min 34:00
+  let message = {
+    content: chatInput.value,
+  };
+  messages.push(message);
+  chatInput.value = "";
+  renderMessages();
+  sendMessages();
+}
+
+function sendMessages() {
+  localStorage.setItem("messages", JSON.stringify(messages));
+}
+
+function renderMessages() {
+  let html = `<ul>`;
+
+  // HW5: rewrite the loop code using array.forEach ()
+  for (let i = Math.max(messages.length - 6, 0); i < messages.length; i++) {
+    html += `<li>${messages[i].content}</li>`;
+  }
+  html += `</ul>`;
+  chatBody.innerHTML = html;
+}
+
+function openChat() {
+  chatWindow.classList.remove("hidden");
+  localStorage.removeItem("chatHidden");
+}
+function closeChat() {
+  chatWindow.classList.add("hidden");
+  localStorage.setItem("chatHidden", true);
+}
